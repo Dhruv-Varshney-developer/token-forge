6efeb0ac255bedaf88b82444b210b354b3343385
Dhruv-Varshney-developer <f20200865@pilani.bits-pilani.ac.in>
2024-07-11 09:15:56 +0530

    burnbatch and mintbatch removed.

--- ERC1155token.sol
+++ ERC1155token.sol
@@ -28,19 +28,13 @@ contract MyERC1155Token is ERC1155,Ownable  {
         _mint(msg.sender, id, amount, "");
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner {
-        _mintBatch(to, ids, amounts, data);
-    }
 
     function burn(address from, uint256 id, uint256 amount) public {
         require(msg.sender == from || isApprovedForAll(from, msg.sender), "Caller is not owner nor approved");
         _burn(from, id, amount);
     }
 
-    function burnBatch(address from, uint256[] memory ids, uint256[] memory amounts) public {
-        require(msg.sender == from || isApprovedForAll(from, msg.sender), "Caller is not owner nor approved");
-        _burnBatch(from, ids, amounts);
-    }
+    
 
     function uri(uint256 id) public view override returns (string memory) {
     return string(abi.encodePacked(super.uri(id),id.toString()));
