7151aa04efd900c62d7a602cb9d0ffdaf917399c
Dhruv-Varshney-developer <f20200865@pilani.bits-pilani.ac.in>
2024-07-22 19:43:25 +0530

    ERC1155 token and forging updated.

--- forging.sol
+++ forging.sol
@@ -5,15 +5,15 @@ import "module-3/ERC1155token.sol";
 
 contract TokenForge {
     MyERC1155Token private tokenContract;
-    address private constant ERC1155_ADDRESS = 0xc01E9EfA9E40B64908dAE732063e841cD6101C9A;
+    address private constant ERC1155_ADDRESS = 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8;
 
     constructor() {
         tokenContract = MyERC1155Token(ERC1155_ADDRESS);
     }
 
     function forgeToken(uint256[] memory burnIds, uint256[] memory burnAmounts, uint256 mintId) public {
-        require(burnIds.length == burnAmounts.length, "Mismatched burn data");
-        require(isValidForge(burnIds, mintId), "Invalid forge");
+        require(burnIds.length == burnAmounts.length, "Mismatched burn data. Please provide burnAmounts for all burnIDs and vice versa.");
+        require(isValidForge(burnIds, mintId), "Invalid forge. Please check forging instructions.");
 
         for (uint256 i = 0; i < burnIds.length; i++) {
             tokenContract.burn(msg.sender, burnIds[i], burnAmounts[i]);
